f618725506a94c37964c013431c1cf893f1c8afe
Megha <meghadhangar1301@gmail.com>
2023-09-20 13:18:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import WetherApp from './WetherApp';
+function App() {
+  return (
+    <div className="App">
+     <WetherApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WetherApp.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import img1 from './image/sr.png';
+import img2 from './image/pic2.png'
+import img3 from './image/clud.jpg'
+
+function WeatherApp() {
+
+// const[city,setcity] =useState();
+// const[search,setsearch]=useState("");
+
+// useEffect(()=>{
+//   const fetchApi = async()=>{
+
+ 
+//   const url =`"https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d9132312364d6842575847c5579bb6c2`;
+//   const response = await fetch(url);
+//   const resJson = await response.json();
+// };
+// fetchApi();
+// })
+
+
+
+
+  const apikey = "d9132312364d6842575847c5579bb6c2"
+  const [data, setData] = useState({})
+  const [inputCity, setInputCity] = useState("")
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+
+  const getWeatherDetail = (cityName) => {
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apikey
+    axios.get(apiURL).then((res) => {
+      console.log("Response", res.data)
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    setInputCity(e.target.value);
+  }
+
+  const handleSearch = () => {
+    getWeatherDetail(inputCity);
+  }
+
+  useEffect(() => {
+    getWeatherDetail("Khategaon");
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className='box'>
+      <div className='mainBg'>
+        <div className='parent'>
+          <input className='input-text' onChange={handleChangeInput} value={inputCity}></input>
+          <button className="btnn" type="button" onClick={handleSearch}>Search</button>
+        </div>
+
+        <div className='second'>
+          <img className="pic" src={img1} alt="Weather Icon" />
+          <div className="third">
+            <h1 className='weather-temp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C</h1>
+            <h2 className='weather-city'>{data.name}</h2>
+            <h5 className='current-time'>{currentTime}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherApp;
