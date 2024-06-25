3f2cb98a127c7236b56bcf5ae5b66fca23629f63
Megha Dhangar <115803399+MeghaDhangar@users.noreply.github.com>
2024-06-25 23:07:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react';  c
 import WetherApp from './WetherApp';
 function App() {
   return (
     <div className="App">
      <WetherApp/>
-    
     </div>
   );
 }
+
+
 export default App;
